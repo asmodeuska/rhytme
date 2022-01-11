9b5bbe428174af5d13d72df4c37890a5402c3c1d
asmodeuska <szaboviktor161@gmail.com>
2022-01-11 14:53:13 +0100

    visualizer

--- script.js
+++ script.js
@@ -1,173 +1,150 @@
-var dataLength;
-var peaks;
-var duration;
+//inspired by https://codepen.io/nelsonr/
 
-//ha nem pontos, akkor ezt lehet állítani
-const treeshold = 0.85;
-//peakek sűrűsége (minél kisebb annál sűrűbb)
-const grouping = 0.0001;
+let canvas = document.querySelector("canvas");
+let ctx = canvas.getContext("2d");
+let file = document.getElementById("upload");
+let audio = document.getElementById("audio");
+let src = document.getElementById("src");
+
+canvas.width = document.body.clientWidth -40;
+canvas.height = 700;
+
+let centerX = canvas.width / 2;
+let centerY = canvas.height / 2;
+let radius = document.body.clientWidth <= 425 ? 120 : 160;
+let steps = document.body.clientWidth <= 425 ? 60 : 120;
+let interval = 360 / steps;
+let pointsUp = [];
+let pointsDown = [];
+let running = false;
+let pCircle = 2 * Math.PI * radius;
+let angleExtra = 90;
+
+let context = null;
+let splitter;
+let analyserL;
+let analyserR;
+let bufferLengthL;
+let bufferLengthR;
+let audioDataArrayL;
+let audioDataArrayR;
+let source = null;
 
 
-//1920px kijelző ajánlott
+// Create points
+for (let angle = 0; angle < 360; angle += interval) {
+    let distUp = 1.1;
+    let distDown = 0.9;
 
+    pointsUp.push({
+        angle: angle + angleExtra,
+        x: centerX + radius * Math.cos((-angle + angleExtra) * Math.PI / 180) * distUp,
+        y: centerY + radius * Math.sin((-angle + angleExtra) * Math.PI / 180) * distUp,
+        dist: distUp
+    });
+
+    pointsDown.push({
+        angle: angle + angleExtra + 5,
+        x: centerX + radius * Math.cos((-angle + angleExtra + 5) * Math.PI / 180) * distDown,
+        y: centerY + radius * Math.sin((-angle + angleExtra + 5) * Math.PI / 180) * distDown,
+        dist: distDown
+    });
+}
 
 window.onload = function () {
-    for (i = 0; i < 400; i++) {
-        document.getElementById("tableTR").insertCell(i);
-    }
-    document.getElementById("upload").addEventListener("change", handleFiles, false);
-    document.getElementById("audio").addEventListener("timeupdate", updateBar, false);
-}
+    file.addEventListener('change', (event) => {
+        var files = event.target.files;
+        src.src = URL.createObjectURL(files[0]);
+        audio.load();
+        
+        context = context || new AudioContext();
+        splitter = splitter || context.createChannelSplitter();
+        analyserL = context.createAnalyser();
+        analyserL.fftSize = 8192;
+        analyserR = context.createAnalyser();
+        analyserR.fftSize = 8192;
+        splitter.connect(analyserL, 0, 0);
+        splitter.connect(analyserR, 1, 0);
+        bufferLengthL = analyserL.frequencyBinCount;
+        audioDataArrayL = new Uint8Array(bufferLengthL);
+        bufferLengthR = analyserR.frequencyBinCount;
+        audioDataArrayR = new Uint8Array(bufferLengthR);
 
-function updateBar() {
-    currentTime = document.getElementById("audio").currentTime;
-    duration = document.getElementById("audio").duration;
-    var elem = document.getElementById("myBar");
-    elem.style.width = ((currentTime / duration) * 100) + "%";
-}
-
-function handleFiles(e) {
-    file = e.target.files[0];
-    $("#src").attr("src", URL.createObjectURL(file));
-
-
-    var reader = new FileReader();
-    var context = new (window.AudioContext || window.webkitAudioContext)();
-    reader.onload = function () {
-        context.decodeAudioData(reader.result, function (buffer) {
-            prepare(buffer);
-        });
-    };
-    reader.readAsArrayBuffer(file);
-}
-
-function prepare(buffer) {
-    var offlineContext = new OfflineAudioContext(1, buffer.length, buffer.sampleRate);
-    var source = offlineContext.createBufferSource();
-    source.buffer = buffer;
-    var filter = offlineContext.createBiquadFilter();
-    filter.type = "lowpass";
-    source.connect(filter);
-    filter.connect(offlineContext.destination);
-    source.start(0);
-    offlineContext.startRendering();
-    offlineContext.oncomplete = function (e) {
-        process(e);
-    };
-}
-
-function process(e) {
-    var filteredBuffer = e.renderedBuffer;
-    var data = filteredBuffer.getChannelData(0);
-    dataLength = data.length;
-    var max = arrayMax(data);
-    var min = arrayMin(data);
-    var threshold = min + (max - min) * treeshold;
-    peaks = getPeaksAtThreshold(data, threshold);
-    var intervalCounts = countIntervalsBetweenNearbyPeaks(peaks);
-    var tempoCounts = groupNeighborsByTempo(intervalCounts);
-    tempoCounts.sort(function (a, b) {
-        return b.count - a.count;
+        source = source || context.createMediaElementSource(audio);
+        source.connect(splitter);
+        splitter.connect(context.destination);
     });
-    if (tempoCounts.length) {
-        document.getElementById("output").innerHTML = tempoCounts[0].tempo + " bpm";
-    }
-    calculatePeaks();
-    document.getElementById("audio").load();
-    document.getElementById("audio").play();
 
-}
-
-function calculatePeaks() {
-    peaksNormalized = [];
-    peaks.forEach(element => {
-        peaksNormalized.push((element / dataLength));
+    audio.addEventListener('playing', (event) => {
+        draw();
     });
-    document.getElementById("tableTR").cells[Math.floor(400 * (peaksNormalized[0]))].style.backgroundColor = "red";
-    for(i=1; i<peaksNormalized.length; i++){
-        console.log(i+"= "+(peaksNormalized[i])+">="+(peaksNormalized[i-1]+grouping)+" ?");
-        if (peaksNormalized[i]>=(peaksNormalized[i-1]+grouping)){
-            document.getElementById("tableTR").cells[Math.floor(400 * (peaksNormalized[i]))].style.backgroundColor = "red";
-        }
+}
+
+
+function drawLine(points) {
+    let origin = points[0];
+
+    ctx.beginPath();
+    ctx.strokeStyle = 'rgba(255,255,255,0.5)';
+    ctx.lineJoin = 'round';
+    ctx.moveTo(origin.x, origin.y);
+
+    for (let i = 0; i < points.length; i++) {
+        ctx.lineTo(points[i].x, points[i].y);
     }
 
-
+    ctx.lineTo(origin.x, origin.y);
+    ctx.stroke();
 }
 
-function getPeaksAtThreshold(data, threshold) {
-    var peaksArray = [];
-    var length = data.length;
-    for (var i = 0; i < length;) {
-        if (data[i] > threshold) {
-            peaksArray.push(i);
-            i += 100;
-        }
-        i++;
+function connectPoints(pointsA, pointsB) {
+    for (let i = 0; i < pointsA.length; i++) {
+        ctx.beginPath();
+        ctx.strokeStyle = 'rgba(255,255,255,0.5)';
+        ctx.moveTo(pointsA[i].x, pointsA[i].y);
+        ctx.lineTo(pointsB[i].x, pointsB[i].y);
+        ctx.stroke();
     }
-    return peaksArray;
 }
 
-function countIntervalsBetweenNearbyPeaks(peaks) {
-    var intervalCounts = [];
-    peaks.forEach(function (peak, index) {
-        for (var i = 0; i < 10; i++) {
-            var interval = peaks[index + i] - peak;
-            var foundInterval = intervalCounts.some(function (intervalCount) {
-                if (intervalCount.interval === interval) return intervalCount.count++;
-            });
-            if (!isNaN(interval) && interval !== 0 && !foundInterval) {
-                intervalCounts.push({
-                    interval: interval,
-                    count: 1
-                });
-            }
-        }
-    });
-    return intervalCounts;
-}
+function update(dt) {
+    let audioIndex, audioValue;
 
-function groupNeighborsByTempo(intervalCounts) {
-    var tempoCounts = [];
-    intervalCounts.forEach(function (intervalCount) {
-        var theoreticalTempo = 60 / (intervalCount.interval / 44100);
-        theoreticalTempo = Math.round(theoreticalTempo);
-        if (theoreticalTempo === 0) {
-            return;
-        }
-        while (theoreticalTempo < 90) theoreticalTempo *= 2;
-        while (theoreticalTempo > 180) theoreticalTempo /= 2;
+    // get the current audio data
+    analyserL.getByteFrequencyData(audioDataArrayL);
+    analyserR.getByteFrequencyData(audioDataArrayR);
 
-        var foundTempo = tempoCounts.some(function (tempoCount) {
-            if (tempoCount.tempo === theoreticalTempo) return tempoCount.count += intervalCount.count;
-        });
-        if (!foundTempo) {
-            tempoCounts.push({
-                tempo: theoreticalTempo,
-                count: intervalCount.count
-            });
-        }
-    });
-    return tempoCounts;
-}
+    for (let i = 0; i < pointsUp.length; i++) {
+        audioIndex = Math.ceil(pointsUp[i].angle * (bufferLengthL / (pCircle * 2))) | 0;
+        // get the audio data and make it go from 0 to 1
+        audioValue = audioDataArrayL[audioIndex] / 255;
 
-function arrayMin(arr) {
-    var len = arr.length,
-        min = Infinity;
-    while (len--) {
-        if (arr[len] < min) {
-            min = arr[len];
-        }
+        pointsUp[i].dist = 1.1 + audioValue * 0.8;
+        pointsUp[i].x = centerX + radius * Math.cos(-pointsUp[i].angle * Math.PI / 180) * pointsUp[i].dist;
+        pointsUp[i].y = centerY + radius * Math.sin(-pointsUp[i].angle * Math.PI / 180) * pointsUp[i].dist;
+
+        audioIndex = Math.ceil(pointsDown[i].angle * (bufferLengthR / (pCircle * 2))) | 0;
+        // get the audio data and make it go from 0 to 1
+        audioValue = audioDataArrayR[audioIndex] / 255;
+
+        pointsDown[i].dist = 0.9 + audioValue * 0.2;
+        pointsDown[i].x = centerX + radius * Math.cos(-pointsDown[i].angle * Math.PI / 180) * pointsDown[i].dist;
+        pointsDown[i].y = centerY + radius * Math.sin(-pointsDown[i].angle * Math.PI / 180) * pointsDown[i].dist;
     }
-    return min;
 }
 
-function arrayMax(arr) {
-    var len = arr.length,
-        max = -Infinity;
-    while (len--) {
-        if (arr[len] > max) {
-            max = arr[len];
-        }
+function draw(dt) {
+    requestAnimationFrame(draw);
+
+    if (!audio.paused) {
+        update(dt);
     }
-    return max;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    drawLine(pointsUp);
+    drawLine(pointsDown);
+    connectPoints(pointsUp, pointsDown);
+    ctx.shadowBlur = 40;
+    ctx.shadowColor = "black";
 }
